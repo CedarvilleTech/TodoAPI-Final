e918d7be49728e5c9df7d8d6603c2b43c914a612
LMaster765 <larsenl1973@gmail.com>
2024-11-21 14:10:30 -0500

    Update completed field of Todo model

--- todo/models.py
+++ todo/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Todo(models.Model):
     task = models.CharField(max_length=200)
-    completed = models.BooleanField(null=True, default=False, blank=True)
+    completed = models.BooleanField(default=False, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
